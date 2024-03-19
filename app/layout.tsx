0dece624af98c7598e8fe0f28349047049139ae9
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2024-03-20 05:04:58 +0700

    prepare deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,42 @@
 import './globals.css';
+import { Metadata } from 'next';
 
 import { GeistSans } from 'geist/font/sans';
 
-let title = 'Next.js + Postgres Auth Starter';
-let description =
-  'This is a Next.js starter kit that uses NextAuth.js for simple email + password login and a Postgres database to persist the data.';
-
-export const metadata = {
-  title,
-  description,
-  twitter: {
-    card: 'summary_large_image',
-    title,
-    description,
+export const metadata: Metadata = {
+  title: "Admin Panel - Citramas",
+  description:
+    "Admin Panel - Citramas",
+  generator: "Next.js",
+  applicationName: "Admin Panel - Citramas",
+  keywords: [
+    "Citramas",
+  ],
+  metadataBase: new URL("http://citramas-foundation.com/admin-panel/"),
+  creator: "Reza Bagus Saputra",
+  alternates: {
+    canonical: "http://citramas-foundation.com/admin-panel/",
   },
-  metadataBase: new URL('https://nextjs-postgres-auth.vercel.app'),
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+    ],
+  },
 };
 
 export default function RootLayout({
--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'export',
   env: {
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
       process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,

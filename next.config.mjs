50e9d23c2cce6868d8e0b508dd74e9c82296c411
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2024-03-22 23:29:40 +0700

    remove output export

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: 'export',
   env: {
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
       process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
